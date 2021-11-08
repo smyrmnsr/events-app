1767454f5ebc5a7632ec31c3e4aad90ec2d18a82
smyrmnsr <63547955+smyrmnsr@users.noreply.github.com>
2021-11-08 14:11:54 +0200

    Redux setup done

--- src/index.js
+++ src/index.js
@@ -3,21 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import ManageEvents from './routes/manage-events';
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
+
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+
+import rootReducer from './store/reducers/rootReducer';
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="manage-events" element={<ManageEvents />} />
-        </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+    <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -26,4 +25,3 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
---
+++ src/store/reducers/eventReducer.js
@@ -0,0 +1,5 @@
+const eventReducer = (state = [], action) => {
+    return state;
+}
+
+export default eventReducer;
